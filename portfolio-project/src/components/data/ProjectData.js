d32ab51216a370ac8b0c61a3f90f32016c195454
Sylwia <sonnenuntergang.email@gmail.com>
2022-03-13 18:20:08 +0100

    feat: add ProjectCard styles

--- portfolio-project/src/components/data/ProjectData.js
+++ portfolio-project/src/components/data/ProjectData.js
@@ -1,27 +1,37 @@
 export const ProjectData = [
     {
         id: 1,
-        title: "Project 1",
+        title: "Survey Form",
         about: "This project I made using...",
         tags: ["React.js", "JavaScript", "HTML", "CSS"],
         demo: "https://status-code-js.github.io/survey-form/",
+        image: "https://api.urlbox.io/v1/ca482d7e-9417-4569-90fe-80f7c5e1c781/5f0becc9bba1f0a9c5798c4604ace9247eaa697e/png?url=https%3A%2F%2Fstatus-code-js.github.io%2Fsurvey-form%2F",
     },
 
     {
         id: 2,
-        title: "Project 2",
+        title: "Slider Sources",
         about: "This project I made using...",
         tags: ["JavaScript", "HTML", "CSS"],
-        demo: "https://status-code-js.github.io/survey-form/",
-
+        demo: "https://status-code-js.github.io/slider-sources/",
+        image: "https://api.urlbox.io/v1/ca482d7e-9417-4569-90fe-80f7c5e1c781/f4d4f71828ea5c7991795ebfc940621aaaaea81a/png?url=https%3A%2F%2Fstatus-code-js.github.io%2Fslider-sources%2F",
     },
     {
-
         id: 3,
-        title: "Project 3",
+        title: "Drag & Drop",
         about: "This project I made using...",
         tags: ["React.js", "JavaScript", "HTML", "CSS"],
-        demo: "https://status-code-js.github.io/survey-form/",
+        demo: "https://status-code-js.github.io/drag-n-drop-sources/",
+        image: "https://api.urlbox.io/v1/ca482d7e-9417-4569-90fe-80f7c5e1c781/04199f5c564f48861a3639002651c0df1c74c9f6/png?url=https%3A%2F%2Fstatus-code-js.github.io%2Fdrag-n-drop-sources%2F",
+    },
+    {
+        id: 4,
+        title: "Card Sources",
+        about: "This project I made using...",
+        tags: ["React.js", "JavaScript", "HTML", "CSS"],
+        demo: "https://status-code-js.github.io/card-sources/",
+        image: "https://api.urlbox.io/v1/ca482d7e-9417-4569-90fe-80f7c5e1c781/191a95c0b04b9b0990d694c93880308455e2f724/png?url=https%3A%2F%2Fstatus-code-js.github.io%2Fcard-sources%2F",
     }
+
 
 ]
